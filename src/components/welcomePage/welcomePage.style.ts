5ce985e9e38fbfa65e1ec7e2391671338c272e2c
Alexandre Do Pham <alexandre34980@outlook.fr>
2021-09-30 13:52:26 -0400

    public folder clean up

--- src/components/welcomePage/welcomePage.style.ts
+++ src/components/welcomePage/welcomePage.style.ts
@@ -2,10 +2,6 @@ import { makeStyles, createStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(thme =>
   ({
-    avatar: {
-      height:'25%',
-      width:'25%',
-    },
     box : {
       margin: 25,
       alignItems:'center',
